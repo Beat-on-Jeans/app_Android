03fde229b81488fc49d69f9e71c9e810051621e9
soyeldet <hugotorrespedraza90@gmail.com>
2025-02-14 20:17:37 +0100

    gallery update

--- app/src/main/java/com/example/prueba_beat_on_jeans/RegisterActivity5.kt
+++ app/src/main/java/com/example/prueba_beat_on_jeans/RegisterActivity5.kt
@@ -1,42 +1,92 @@
 package com.example.prueba_beat_on_jeans
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
-import android.widget.GridView
 import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class RegisterActivity5 : AppCompatActivity() {
+
+    private val PICK_IMAGES_REQUEST_CODE = 1
+
+    private val imageList = MutableList<Uri?>(12) { null }
+
+    private lateinit var galleryAdapter: GalleryAdapter
+
+    private val photoList = ArrayList<Uri>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_register5)
 
-        val gridViewImagese: GridView = findViewById(R.id.images_grid)
+        val recyclerViewImages: RecyclerView = findViewById(R.id.images_grid)
         val buttonContinue: Button = findViewById(R.id.continue_button)
-        val imagebuttonBack: ImageButton = findViewById(R.id.back_imageButton)
+        val imageButtonBack: ImageButton = findViewById(R.id.back_imageButton)
 
-        gridViewImagese.setOnClickListener {
-
+        galleryAdapter = GalleryAdapter(imageList) { position ->
+            Toast.makeText(this, "Item $position clickeado", Toast.LENGTH_SHORT).show()
+            openGallery()
         }
+
+        recyclerViewImages.layoutManager = GridLayoutManager(this, 3) // 3 columnas en el grid
+        recyclerViewImages.adapter = galleryAdapter
 
         buttonContinue.setOnClickListener {
             val intent = Intent(this, LogInActivity::class.java)
             startActivity(intent)
         }
 
-        imagebuttonBack.setOnClickListener{
+        imageButtonBack.setOnClickListener {
             val intent = Intent(this, RegisterActivity4::class.java)
             startActivity(intent)
         }
-
     }
 
-}
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+            type = "image/*"
+            putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+        }
+        startActivityForResult(
+            Intent.createChooser(intent, "Selecciona imágenes"),
+            PICK_IMAGES_REQUEST_CODE
+        )
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGES_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val newList = mutableListOf<Uri?>()
+
+            data?.let {
+                if (it.clipData != null) {
+                    val count = it.clipData!!.itemCount
+                    for (i in 0 until count) {
+                        if (newList.size < 12) {
+                            val imageUri = it.clipData!!.getItemAt(i).uri
+                            newList.add(imageUri)
+                        }
+                    }
+                } else if (it.data != null) {
+                    newList.add(it.data!!)
+                }
+            }
+
+            while (newList.size < 12) {
+                newList.add(null)
+            }
+
+            imageList.clear()
+            imageList.addAll(newList)
+            galleryAdapter.notifyDataSetChanged()
+        }
+    }
+}
