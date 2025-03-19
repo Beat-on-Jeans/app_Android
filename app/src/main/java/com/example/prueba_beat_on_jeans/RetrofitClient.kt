be1d921ea4f24eb8e9aaf50e98ec7fdb2c185779
soyeldet <hugotorrespedraza90@gmail.com>
2025-03-19 15:48:11 +0100

    Log in

--- app/src/main/java/com/example/prueba_beat_on_jeans/RetrofitClient.kt
+++ app/src/main/java/com/example/prueba_beat_on_jeans/RetrofitClient.kt
@@ -6,6 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClient {
     private const val BASE_URL = "http://10.0.0.99/dam05/"
 
+
     val instance: ApiService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
