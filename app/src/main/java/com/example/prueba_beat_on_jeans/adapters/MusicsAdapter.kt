d9f29f44df6224abedb6e594988eccf701062458
Helio Verdugo <hverdugob2425@politecnics.barcelona>
2025-04-01 16:13:29 +0200

    Adaptación de Matches

--- app/src/main/java/com/example/prueba_beat_on_jeans/adapters/MusicsAdapter.kt
+++ app/src/main/java/com/example/prueba_beat_on_jeans/adapters/MusicsAdapter.kt
@@ -1,4 +1,4 @@
-package com.example.prueba_beat_on_jeans.adapters
+package com.example.prueba_beat_on_jeans
 
 import android.annotation.SuppressLint
 import android.app.Activity
@@ -12,8 +12,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.prueba_beat_on_jeans.api.Matches
-import com.example.prueba_beat_on_jeans.R
+import coil.load
 import com.yuyakaido.android.cardstackview.CardStackLayoutManager
 import com.yuyakaido.android.cardstackview.CardStackListener
 import com.yuyakaido.android.cardstackview.CardStackView
@@ -31,7 +30,7 @@ class MusicsAdapter(
     inner class MusicViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val nameTextView: TextView = view.findViewById(R.id.TxtName)
         val descriptionTextView: TextView = view.findViewById(R.id.TxtDescription)
-        val imageMusicians: FrameLayout = view.findViewById(R.id.RVImgBackGround)
+        val imageMusicians: ImageView = view.findViewById(R.id.RVImgBackGround)
         val rvTag: RecyclerView = view.findViewById(R.id.RVTags)
         val likeButton: ImageButton = view.findViewById(R.id.BtnHeart)
         val talkButton: ImageButton = view.findViewById(R.id.BtnTalk)
@@ -50,7 +49,9 @@ class MusicsAdapter(
         val music = matchesList[position]
         holder.nameTextView.text = music.name
         holder.descriptionTextView.text = music.description
-        holder.imageMusicians.setBackgroundResource(R.drawable.human)
+        holder.imageMusicians.load(music.img) {
+            crossfade(true)
+        }
 
         val adapter = TagsAdapter(music.arrayTags)
         holder.rvTag.adapter = adapter
@@ -103,6 +104,16 @@ class MusicsAdapter(
             holder.leftOverlay.visibility = View.INVISIBLE
             holder.rightOverlay.visibility = View.INVISIBLE
             holder.backColor.alpha = 0.0f
+        }
+        if (direction == Direction.Right) {
+            val layoutManager = (context as? Activity)?.findViewById<CardStackView>(R.id.CVMusicians)?.layoutManager
+            if (layoutManager is CardStackLayoutManager) {
+                val topPosition = layoutManager.topPosition - 1 // Se resta 1 porque la carta ya se movió
+                if (topPosition in matchesList.indices) {
+                    val userLiked = matchesList[topPosition]
+                    onLikeClick(userLiked) // Llamamos a la acción de "Like"
+                }
+            }
         }
     }
 
